ac4f20e26d3627a6839e9558c637a2dafb657afd
biar <niti2539@gmail.com>
2016-10-14 22:32:35 +0700

    aaaa

---
+++ sometest/List.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test2;
+
+/**
+ *
+ * @author usci
+ */
+public interface List extends Collection{
+      public void add(int i,Object e);
+    public void remove(int i);
+    public Object get(int i);
+    public void set (int i,Object e);
+    
+}
